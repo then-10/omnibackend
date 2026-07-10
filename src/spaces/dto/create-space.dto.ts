e0cc871dbc42ac1b098874f9859ad54299ed7ca8
then-10 <jaredthen+git@gmail.com>
2026-07-10 13:34:49 -0600

    Add CreateSpaceDto with validation rules

--- src/spaces/dto/create-space.dto.ts
+++ src/spaces/dto/create-space.dto.ts
@@ -1,1 +1,14 @@
+import { IsString, IsOptional, MinLength, IsUUID } from 'class-validator';
 
+export class CreateSpaceDto {
+  @IsString()
+  @MinLength(3)
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsUUID()
+  workspaceId: string;
+}
