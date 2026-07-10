fae154d4dd7723dc5a1c5d8a4f1fc94029011c9a
then-10 <jaredthen+git@gmail.com>
2026-07-10 13:29:45 -0600

    Add findById method in UsersService
    Implemented findById method to retrieve user details by ID.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,1 +1,27 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async findById(id: string) {
+    const user = await this.prisma.user.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        avatarUrl: true,
+        createdAt: true,
+        workspaces: {
+          include: {
+            workspace: true,
+          },
+        },
+      },
+    });
+    if (!user) throw new NotFoundException('User not found');
+    return user;
+  }
+}
