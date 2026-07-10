8e5d4debd5c80e5ff4147a27277876e9f7c14c43
then-10 <jaredthen+git@gmail.com>
2026-07-10 13:39:22 -0600

    Add CreateListDto with validation decorators

--- src/lists/dto/create-list.dto.ts
+++ src/lists/dto/create-list.dto.ts
@@ -1,1 +1,24 @@
+import { IsString, IsOptional, MinLength, IsUUID, IsInt, Min } from 'class-validator';
 
+export class CreateListDto {
+  @IsString()
+  @MinLength(3)
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsUUID()
+  @IsOptional()
+  spaceId?: string;
+
+  @IsUUID()
+  @IsOptional()
+  folderId?: string;
+
+  @IsInt()
+  @IsOptional()
+  @Min(0)
+  position?: number;
+}
