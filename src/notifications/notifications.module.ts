c20da9c25c7832a1a70cb2a42c82a86f75aa0cf2
then-10 <jaredthen+git@gmail.com>
2026-07-10 13:48:27 -0600

    Add NotificationsModule with JWT configuration

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,1 +1,19 @@
+import { Module } from '@nestjs/common';
+import { NotificationsGateway } from './notifications.gateway';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
+@Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        secret: config.get('JWT_SECRET'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [NotificationsGateway],
+  exports: [NotificationsGateway],
+})
+export class NotificationsModule {}
