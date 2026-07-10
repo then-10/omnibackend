e3757e16ecef9d66e55bdc7a9a065a2a03a4fb7b
then-10 <jaredthen+git@gmail.com>
2026-07-10 13:24:01 -0600

    Add modules and configuration to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,1 +1,38 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { WorkspacesModule } from './workspaces/workspaces.module';
+import { SpacesModule } from './spaces/spaces.module';
+import { FoldersModule } from './folders/folders.module';
+import { ListsModule } from './lists/lists.module';
+import { TasksModule } from './tasks/tasks.module';
+import { CommentsModule } from './comments/comments.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { SearchModule } from './search/search.module';
+import { AutomationModule } from './automation/automation.module';
+import { PrismaService } from './prisma/prisma.service';
 
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot([{
+      ttl: 60,
+      limit: 100,
+    }]),
+    AuthModule,
+    UsersModule,
+    WorkspacesModule,
+    SpacesModule,
+    FoldersModule,
+    ListsModule,
+    TasksModule,
+    CommentsModule,
+    NotificationsModule,
+    SearchModule,
+    AutomationModule,
+  ],
+  providers: [PrismaService],
+})
+export class AppModule {}
