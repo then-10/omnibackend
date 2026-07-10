44fd418754359b425577bc953fe23feba5452a66
then-10 <jaredthen+git@gmail.com>
2026-07-10 13:49:16 -0600

    Add SearchModule with Elasticsearch integration

--- src/search/search.module.ts
+++ src/search/search.module.ts
@@ -1,1 +1,19 @@
+import { Module } from '@nestjs/common';
+import { ElasticsearchModule } from '@nestjs/elasticsearch';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SearchService } from './search.service';
 
+@Module({
+  imports: [
+    ElasticsearchModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (config: ConfigService) => ({
+        node: config.get('ELASTICSEARCH_URL') || 'http://localhost:9200',
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [SearchService],
+  exports: [SearchService],
+})
+export class SearchModule {}
