5d3ebd230c133188186f3c1e9ae9146b2e81a402
then-10 <jaredthen+git@gmail.com>
2026-07-10 13:35:10 -0600

    Add validation to UpdateSpaceDto properties

--- src/spaces/dto/update-space.dto.ts
+++ src/spaces/dto/update-space.dto.ts
@@ -1,1 +1,12 @@
+import { IsString, IsOptional, MinLength } from 'class-validator';
 
+export class UpdateSpaceDto {
+  @IsString()
+  @IsOptional()
+  @MinLength(3)
+  name?: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+}
