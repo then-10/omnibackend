aaf607b46234e9f247010f086adad111c3a37967
then-10 <jaredthen+git@gmail.com>
2026-07-10 13:30:33 -0600

    Create WorkspacesModule with controller and services

--- src/workspaces/workspaces.module.ts
+++ src/workspaces/workspaces.module.ts
@@ -1,1 +1,11 @@
+import { Module } from '@nestjs/common';
+import { WorkspacesController } from './workspaces.controller';
+import { WorkspacesService } from './workspaces.service';
+import { PrismaService } from '../prisma/prisma.service';
 
+@Module({
+  controllers: [WorkspacesController],
+  providers: [WorkspacesService, PrismaService],
+  exports: [WorkspacesService],
+})
+export class WorkspacesModule {}
