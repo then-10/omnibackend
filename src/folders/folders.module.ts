e12e1795cafb634df233efa0eba9cfd50997f9af
then-10 <jaredthen+git@gmail.com>
2026-07-10 13:35:43 -0600

    Create FoldersModule with controller and service

--- src/folders/folders.module.ts
+++ src/folders/folders.module.ts
@@ -1,1 +1,12 @@
+import { Module } from '@nestjs/common';
+import { FoldersController } from './folders.controller';
+import { FoldersService } from './folders.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { SpacesModule } from '../spaces/spaces.module';
 
+@Module({
+  imports: [SpacesModule],
+  controllers: [FoldersController],
+  providers: [FoldersService, PrismaService],
+})
+export class FoldersModule {}
