b4e44648fa25f33987e90592d252c6c51deb517c
then-10 <jaredthen+git@gmail.com>
2026-07-10 13:31:57 -0600

    Add validation to CreateWorkspaceDto

--- src/workspaces/dto/create-workspace.dto.ts
+++ src/workspaces/dto/create-workspace.dto.ts
@@ -1,1 +1,11 @@
+import { IsString, IsOptional, MinLength } from 'class-validator';
 
+export class CreateWorkspaceDto {
+  @IsString()
+  @MinLength(3)
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+}
