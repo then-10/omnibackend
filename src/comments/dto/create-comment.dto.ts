7ced0c11016463ce5237bd6a417d784e7031b357
then-10 <jaredthen+git@gmail.com>
2026-07-10 13:48:00 -0600

    Add CreateCommentDto with validation rules

--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,1 +1,10 @@
+import { IsString, IsUUID, MinLength } from 'class-validator';
 
+export class CreateCommentDto {
+  @IsString()
+  @MinLength(1)
+  content: string;
+
+  @IsUUID()
+  taskId: string;
+}
