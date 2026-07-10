089f1aa212305367b4dd253ed86cedef71b2210c
then-10 <jaredthen+git@gmail.com>
2026-07-10 13:46:56 -0600

    Add CommentsModule with controller and service

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -1,1 +1,12 @@
+import { Module } from '@nestjs/common';
+import { CommentsController } from './comments.controller';
+import { CommentsService } from './comments.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { TasksModule } from '../tasks/tasks.module';
 
+@Module({
+  imports: [TasksModule],
+  controllers: [CommentsController],
+  providers: [CommentsService, PrismaService],
+})
+export class CommentsModule {}
