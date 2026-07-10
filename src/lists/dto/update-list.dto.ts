35134a84c4f588b3e48b80b7f809ab3519dc3903
then-10 <jaredthen+git@gmail.com>
2026-07-10 13:39:48 -0600

    Add validation to UpdateListDto properties

--- src/lists/dto/update-list.dto.ts
+++ src/lists/dto/update-list.dto.ts
@@ -1,1 +1,17 @@
+import { IsString, IsOptional, MinLength, IsInt, Min } from 'class-validator';
 
+export class UpdateListDto {
+  @IsString()
+  @IsOptional()
+  @MinLength(3)
+  name?: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsInt()
+  @IsOptional()
+  @Min(0)
+  position?: number;
+}
