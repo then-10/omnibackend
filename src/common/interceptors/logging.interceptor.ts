5628779a27e03273210ce5680ed0cb240d6a60b7
then-10 <jaredthen+git@gmail.com>
2026-07-10 13:52:48 -0600

    Add LoggingInterceptor for request logging
    Implement a logging interceptor to log request details and duration.

--- src/common/interceptors/logging.interceptor.ts
+++ src/common/interceptors/logging.interceptor.ts
@@ -1,1 +1,18 @@
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const req = context.switchToHttp().getRequest();
+    const { method, url } = req;
+    const now = Date.now();
+
+    return next.handle().pipe(
+      tap(() => {
+        console.log(`${method} ${url} - ${Date.now() - now}ms`);
+      }),
+    );
+  }
+}
