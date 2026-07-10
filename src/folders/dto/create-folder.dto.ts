4c390bb2d8f47d4e8902cb37c2025d7b1194fa85
then-10 <jaredthen+git@gmail.com>
2026-07-10 13:36:53 -0600

    Add CreateFolderDto with validation rules

--- src/folders/dto/create-folder.dto.ts
+++ src/folders/dto/create-folder.dto.ts
@@ -1,1 +1,14 @@
+import { IsString, IsOptional, MinLength, IsUUID } from 'class-validator';
 
+export class CreateFolderDto {
+  @IsString()
+  @MinLength(3)
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsUUID()
+  spaceId: string;
+}
