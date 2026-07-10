7821923365dee8915547944b802abdf7954b7b92
then-10 <jaredthen+git@gmail.com>
2026-07-10 13:37:18 -0600

    Add UpdateFolderDto with validation rules

--- src/folders/dto/update-folder.dto.ts
+++ src/folders/dto/update-folder.dto.ts
@@ -1,1 +1,12 @@
+import { IsString, IsOptional, MinLength } from 'class-validator';
 
+export class UpdateFolderDto {
+  @IsString()
+  @IsOptional()
+  @MinLength(3)
+  name?: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+}
