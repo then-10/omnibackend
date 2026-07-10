a6a0ed0ce0480348ee8cfada9b22dfa91ad77da1
then-10 <jaredthen+git@gmail.com>
2026-07-10 13:29:23 -0600

    Add UsersController with JWT authentication

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,1 +1,19 @@
+import { Controller, Get, Param, UseGuards, Req } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
 
+@Controller('users')
+@UseGuards(JwtAuthGuard)
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get('me')
+  async getMe(@Req() req) {
+    return this.usersService.findById(req.user.userId);
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.usersService.findById(id);
+  }
+}
