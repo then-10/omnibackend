2fdaf14b7525f50f517e5a046dd540b721d9b287
then-10 <jaredthen+git@gmail.com>
2026-07-10 13:38:00 -0600

    Create ListsModule with dependencies and controllers

--- src/lists/lists.module.ts
+++ src/lists/lists.module.ts
@@ -1,1 +1,13 @@
+import { Module } from '@nestjs/common';
+import { ListsController } from './lists.controller';
+import { ListsService } from './lists.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { FoldersModule } from '../folders/folders.module';
+import { SpacesModule } from '../spaces/spaces.module';
 
+@Module({
+  imports: [FoldersModule, SpacesModule],
+  controllers: [ListsController],
+  providers: [ListsService, PrismaService],
+})
+export class ListsModule {}
