37fb56e3f0ea4ae6455c0bcb740625a9b041dec8
then-10 <jaredthen+git@gmail.com>
2026-07-10 13:44:41 -0600

    Add TasksModule with necessary imports and providers

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,1 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TasksController } from './tasks.controller';
+import { TasksService } from './tasks.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { ListsModule } from '../lists/lists.module';
+import { NotificationsModule } from '../notifications/notifications.module';
+import { AutomationModule } from '../automation/automation.module';
 
+@Module({
+  imports: [ListsModule, NotificationsModule, AutomationModule],
+  controllers: [TasksController],
+  providers: [TasksService, PrismaService],
+})
+export class TasksModule {}
